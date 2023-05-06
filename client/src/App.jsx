9bd47f0bbac342206314b1d92e68b0ce85fc2275
unfoldedRose <ptest5557@gmail.com>
2023-05-06 13:06:30 +0530

    initital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import PlacesPage from "./pages/PlacesPage";
+import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacePage from "./pages/PlacePage";
+import BookingsPage from "./pages/BookingsPage";
+import BookingPage from "./pages/BookingPage";
+import AccountPage from "./AccountNav";
+
+ axios.defaults.baseURL = "http://localhost:4000/"
+
+ axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />\
+          <Route path="/account/:subpage?" element={<AccountPage />} />
+          {/* <Route path="/account/:subpage/:action" element={<AccountPage />} /> */}
+          <Route path="/account/places" element={<PlacesPage />} />
+          {/* <Route path="/account" element={<ProfilePage />} /> */}
+          
+          {/* <Route path="/account/places/action" element={<AccountPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id" element={<PlacePage />} /> */}
+          {/* <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />  */}
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App
